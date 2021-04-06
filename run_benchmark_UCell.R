031c728941aed86eaba91326aaa2895e25a8592b
Massimo Andreatta <massimo.andreatta@unil.ch>
2021-04-06 12:45:55 +0200

    Wrappers for time/memory benchmarks

--- run_benchmark_UCell.R
+++ run_benchmark_UCell.R
@@ -5,22 +5,26 @@ method <- args[1]
 size <- as.numeric(args[2])
 ram <- args[3]
 datapath <- args[4]
+outfile <- args[5]
 #for MA: /Users/mass/Documents/Projects/Cell_clustering/UCell_demo/data/sub
+seed <- as.numeric(args[6])
+
 
 chunk.size <- 1000
 force.gc <- FALSE
+
 
 Sys.setenv("R_MAX_VSIZE" = ram)
 print(Sys.getenv("R_MAX_VSIZE"))
 
 #Use these signatures
-features <- list()
-features$Tcell_CD4 <- c("CD4","CD40LG")
-features$Tcell_CD8 <- c("CD8A","CD8B")
-features$Tcell_Treg <- c("FOXP3","IL2RA")
-features$Tcell_MAIT <- c("KLRB1", "SLC4A10", "NCR3")
-features$Tcell_gd <- c("TRDC", "TRGC1", "TRGC2", "TRDV1")
-features$Tcell_NK <- c("FGFBP2", "SPON2", "KLRF1", "FCGR3A", "KLRD1", "TRDC")
+ft <- list()
+ft$Tcell_CD4 <- c("CD4","CD40LG")
+ft$Tcell_CD8 <- c("CD8A","CD8B")
+ft$Tcell_Treg <- c("FOXP3","IL2RA")
+ft$Tcell_MAIT <- c("KLRB1", "SLC4A10", "NCR3")
+ft$Tcell_gd <- c("TRDC", "TRGC1", "TRGC2", "TRDV1")
+ft$Tcell_NK <- c("FGFBP2", "SPON2", "KLRF1", "FCGR3A", "KLRD1", "TRDC")
 
 #Read in data (pre-subset in a different script)
 matrix.sub <- sprintf("%s/pbmc.expmat.%s.rds", datapath, size)
@@ -29,6 +33,22 @@ matrix.sub <- sprintf("%s/pbmc.expmat.%s.rds", datapath, size)
 this.data <- readRDS(matrix.sub)
 
 
+#"CD4" %in% rownames(this.data)
+#"CD4" %in% rownames(this.data)
+#setdiff(unlist(ft), rownames(this.data))
+
+
+
+#Initialize results table
+results <- data.frame(Method=character(),
+                 Size=numeric(), 
+                 Machine_RAM=character(), 
+                 Seed=numeric(),
+                 PeakMemory=numeric(),
+                 Time=numeric())
+
+set.seed(seed)
+
 if (method == "UCell") {
   library(UCell)
   
@@ -36,7 +56,8 @@ if (method == "UCell") {
   t <- system.time({
     
     out <- tryCatch({
-      scores_UCell <- UCell::ScoreSignatures_UCell(this.data, features = features, chunk.size = chunk.size, force.gc = force.gc)
+      scores_UCell <- UCell::ScoreSignatures_UCell(this.data, features = ft, chunk.size = chunk.size, force.gc = force.gc)
+      write.table(head(scores_UCell), "aux/UCell.tmp.scores.txt", quote=F)
       1
     },
     error=function(cond) {
@@ -59,8 +80,9 @@ if (method == "UCell") {
     
     out <- tryCatch({
       cells_rankings <- AUCell::AUCell_buildRankings(this.data, nCores = 1, plotStats = F)
-      cells_AUC <- AUCell::AUCell_calcAUC(features, cells_rankings, aucMaxRank=1000)
+      cells_AUC <- AUCell::AUCell_calcAUC(ft, cells_rankings, aucMaxRank=1000)
       scores_AUC <- as.data.frame(t(AUCell::getAUC(cells_AUC)))
+    #  write.table(head(scores_AUC), "aux/AUCell.tmp.scores.txt", quote=F)
       1
     },
     error=function(cond) {
@@ -79,6 +101,13 @@ if (method == "UCell") {
   }
 }
 
-to.return <- c(time, memPeak)
-print(to.return)
+to.return <- c(method, size, ram, seed, memPeak, time)
+
+write(paste(to.return, collapse = "\t"), outfile, append=TRUE)
+
+
+
+
+
+
 
--- wrapper_benchmark_UCell.R
+++ wrapper_benchmark_UCell.R
@@ -10,10 +10,27 @@ library(Matrix)
 
 script <- "run_benchmark_UCell.R"
 
-method <- "UCell"  # args[0]
-size <- 1000 # args[1]
-ram <- "8Gb" # args[2]
-datapath <- "/Users/mass/Documents/Projects/Cell_clustering/UCell_demo/data/sub" # args[3]
+method <- "UCell"  # args[1]
+ram <- "8Gb" # args[3]
+datapath <- "/Users/mass/Documents/Projects/Cell_clustering/UCell_demo/data/sub" # args[4]
+outfile <- sprintf("%s/bench_results/%s.%s.bench.test.txt", dir, method, ram)  # args[5]
+#size <- 1000 # args[2]
+#seed <- 1 #args[6]
 
-command <- sprintf("Rscript --vanilla %s %s %s %s %s", script, method, size, ram, datapath)
-system(command)
+sizes <- c(100, 200, 500, 1000, 2000, 5000, 1e4, 2e4, 5e4, 1e5)
+seeds <- c(1,2,3,4,5)
+
+columns <- c("Method", "Size", "Machine_RAM", "Seed", "PeakMemory", "Time")
+write(paste(columns, collapse = "\t"), outfile)
+
+for (size in sizes) {
+	for (seed in seeds) {
+
+		command <- sprintf("Rscript --vanilla %s %s %s %s %s %s %s", script, method, size, ram, datapath, outfile, seed)
+
+		print("Executing:")
+		print(command)		
+
+		system(command)
+	}
+}
